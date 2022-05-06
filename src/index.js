aac87b2b6773cd6a186212827de3beffebfe6c4b
dainius1984 <44654388+dainius1984@users.noreply.github.com>
2022-05-06 09:20:13 +0200

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-      <h1>Hello there</h1>
+      <h1>Hello there!!!</h1>
   </React.StrictMode>
 );
 
